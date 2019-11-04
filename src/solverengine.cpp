840547bcbd00bc2700a79e06f7fe3fd10200ac7b
Ben Prisby <ben@benprisby.com>
2019-11-04 09:36:39 -0500

    Remove an unused #include.

--- src/solverengine.cpp
+++ src/solverengine.cpp
@@ -11,7 +11,6 @@
 #include <QDebug>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include <QtMath>
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 /**
